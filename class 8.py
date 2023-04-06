6b447ac45d1214755091c59142690aa620a8bc37
“mmrpondith” <itibdd@gmail.com>
2023-04-06 16:18:31 +0600

    .

---
+++ class 8.py
@@ -0,0 +1,23 @@
+a = "Hello, World!"
+print(a.upper())
+a = "Hello, World!"
+print(a.lower())
+a = " Hello, World! "
+print(a.strip())
+a = "Hello, World!"
+print(a.replace("H", "J"))
+a = "Hello, World!"
+b = a.split(",")
+print(b)
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
---
+++ class7.py
@@ -0,0 +1,34 @@
+
+#years = 21
+#month = 5
+#day = 15
+#myorder = "I want to day {2} for {0} years of item {1} monthe."
+#print(myorder.format(years, month, day))
+
+#txt = 'It\'s alright.'
+#print(txt)
+
+#a=input('Father\'s Nmae')
+#print(a)
+
+#
+#txt = "This will insert one \\\ (backslash)."
+#print(txt)
+
+#txt ="Hello \nWorld! \nnajma \nab \njamal"
+#print(txt)
+
+#txt = "Hello\r World!"
+#print(txt)
+
+#This example erases one character (backspace):
+#txt = "Hello \b World!"
+#print(txt) 
+t = "Hello't' World!"
+print(t) 
+​
+
+
+
+
+
